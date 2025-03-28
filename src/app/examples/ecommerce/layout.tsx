34eb94f6d52b56c7d2c935f78fa21374a28c8242
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2025-03-29 00:45:52 +0300

    delete unnecessary pages

--- src/app/examples/ecommerce/layout.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { type ReactNode } from "react";
-import DashboardLayout from "@/app/(dashboard)/layout";
-
-export default function EcommerceLayout({ children }: { children: ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
